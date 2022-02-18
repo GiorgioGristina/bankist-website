71aca22df57ae7efa1e7be18c4a91e7826d4b9f6
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-02-18 11:37:23 +0000

    event listener for operation tabs and related content content

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// selector for 'learn more' button
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+// tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 
 
@@ -62,3 +67,20 @@ document.querySelector('.nav__links').addEventListener
 
 
 
+
+
+tabsContainer.addEventListener('click', function(e){
+  const clicked = e.target.closest('.btn');
+  // guard clause
+  if(!clicked) return;
+  // active tab
+  tabs.forEach((el) => 
+  el.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+  // active content area
+  tabsContent.forEach((el) => 
+  el.classList.remove('operations__content--active'));
+  document.querySelector(`.operations__content--${clicked
+    .dataset.tab}`).classList.add('operations__content--active')
+  
+})
